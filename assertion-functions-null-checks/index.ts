e9bf80e4b8a5319692f7b78e8c93dec9d95de20a
Ramon Mello <ramonomello@gmail.com>
2022-02-12 01:34:09 -0300

    add assertion functions null checks

---
+++ assertion-functions-null-checks/index.ts
@@ -0,0 +1,13 @@
+function assertIsNonNullish<T>(
+  value: T,
+  message: string
+): asserts value is NonNullable<T> {
+  if (value === null || value === undefined) {
+    throw Error(message);
+  }
+}
+
+const root = document.getElementById("root");
+assertIsNonNullish(root, "Couldn't find DOM element #root");
+
+root.addEventListener("Click", (e) => {});
